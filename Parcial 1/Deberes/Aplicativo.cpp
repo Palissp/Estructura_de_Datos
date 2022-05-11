05e1c3a0cc8d8abb9ee87d2a1e921c063d35068f
Jorge652 <90517513+Jorge652@users.noreply.github.com>
2022-05-10 19:11:05 -0500

    Add files via upload

---
+++ Parcial 1/Deberes/Aplicativo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Ingresar.cpp"
+
+
+int main(int argc, char** argv) {
+	Ingresar o;
+/*	string num;
+	num=o.ingresar("Ingrese el numero:");
+	cout<<"\nNumero transformado: "<<stoi(num);*/
+	o.transformar();
+	return 0;
+}
